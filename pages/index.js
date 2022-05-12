99f541f4d3632c31c4cfbdc1436106638a9cce31
amolmugnusmare <amol20@navgurukul.org>
2022-05-12 16:57:51 +0530

    input redirect using router

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,18 @@ import Header from '../Components/Header';
 import Image from 'next/image';
 import { SearchIcon, MicrophoneIcon } from "@heroicons/react/solid";
 import Footer from '../Components/Footer';
+import { useRouter } from 'next/router';
+import { useRef } from 'react';
 
 export default function Home() {
+  const router = useRouter()
+  const searchInputRef =useRef(null)
+  function search(event){
+    event.preventDefault();
+    const term = searchInputRef.current.value;
+    if(!term.trim())return;
+    router.push(`/search?term=${term.trim()}`)
+  }
   return (
     <div >
       <Head>
@@ -24,14 +34,14 @@ export default function Home() {
           <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gary-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
             <SearchIcon className="h-5 text-gray-500 mr-3" />
             <input
-              
+              ref={searchInputRef}
               type="text"
               className="flex-grow focus:outline-none"
             />
             <MicrophoneIcon className="h-5" />
           </div>
           <div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
-            <button className="btn">
+            <button className="btn" onClick={search}>
               Google Search
             </button>
             <button className="btn">
