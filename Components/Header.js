5cafb3972e09626bc64b77e76b123a23c78bd559
amolmugnusmare <amol20@navgurukul.org>
2022-05-12 15:35:39 +0530

    add authentication and user data

--- Components/Header.js
+++ Components/Header.js
@@ -3,7 +3,7 @@ import User from './User'
 
 export default function Header() {
   return (
-    <header className="flex justify-between p-5 text-sm text-gray-700">
+    <div className="flex justify-between p-5 text-sm text-gray-700">
         <div className='flex space-x-4 items-center'>
             <p className="link">About</p>
             <p className="link">Store</p>
@@ -13,6 +13,6 @@ export default function Header() {
             <p className="link">Images</p>
             <User/>
         </div>
-    </header>
+    </div>
   )
 }
