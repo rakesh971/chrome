abbecb593cafa9bd16df708cd7984d0cf870b0bb
amolmugnusmare <amol20@navgurukul.org>
2022-05-13 11:57:29 +0530

    create data section and title search

--- Response.js
+++ Response.js
@@ -1,66 +1,248 @@
 export default {
-    "kind": "customsearch#result",
-    "title": "Queer Unicorn — Transgender Care Bear",
-    "htmlTitle": "Queer Unicorn — Transgender Care Bear",
-    "link": "https://helloundefined.tumblr.com/post/159394388682/transgender-care-bear",
-    "displayLink": "helloundefined.tumblr.com",
-    "snippet": "Apr 9, 2017 ... p1ss4ss liked this. mintbunny2theface liked this. helloundefined posted this. Show more notesLoading... See more posts like this on Tumblr.",
-    "htmlSnippet": "Apr 9, 2017 <b>...</b> p1ss4ss liked this. mintbunny2theface liked this. <b>helloundefined</b> posted this. Show more notesLoading... See more posts like this on Tumblr.",
-    "cacheId": "eHtmYD_ymVEJ",
-    "formattedUrl": "https://helloundefined.tumblr.com/post/.../transgender-care-bear",
-    "htmlFormattedUrl": "https://<b>helloundefined</b>.tumblr.com/post/.../transgender-care-bear",
-    "pagemap": {
-        "cse_thumbnail": [
+    "kind": "customsearch#search",
+    "url": {
+        "type": "application/json",
+        "template": "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json"
+    },
+    "queries": {
+        "request": [
             {
-                "src": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQfE0_M1_TrrBPJcvVl_pju4wsJw4eDs1lqUitbQbsJartTsw2YKG7Fo9Q",
-                "width": "228",
-                "height": "221"
+                "title": "Google Custom Search - tailwind css",
+                "totalResults": "29200000",
+                "searchTerms": "tailwind css",
+                "count": 10,
+                "startIndex": 1,
+                "inputEncoding": "utf8",
+                "outputEncoding": "utf8",
+                "safe": "off",
+                "cx": "897d475f0e4c4881d",
+                "searchType": "image"
             }
         ],
-        "metatags": [
+        "nextPage": [
             {
-                "og:determiner": "a",
-                "apple-itunes-app": "app-id=305343404,affiliate-data=at=11l3M5&ct=us.itunes.uni.tumblr.ipdt,app-argument=tumblr://x-callback-url/blog?blogName=helloundefined&postID=159394388682&referrer=smart-app-banner",
-                "og:image": "https://64.media.tumblr.com/14a44eb3df693dc2d0091c3751a81c9e/tumblr_oo63nexuWw1vgg8gao1_640.png",
-                "twitter:app:id:googleplay": "com.tumblr",
-                "twitter:app:url:iphone": "tumblr://x-callback-url/blog?blogName=helloundefined&postID=159394388682&referrer=twitter-cards",
-                "theme-color": "#35465D",
-                "og:image:width": "592",
-                "twitter:card": "summary_large_image",
-                "og:site_name": "Queer Unicorn",
-                "twitter:url": "https://helloundefined.tumblr.com/post/159394388682/transgender-care-bear",
-                "twitter:app:url:ipad": "tumblr://x-callback-url/blog?blogName=helloundefined&postID=159394388682&referrer=twitter-cards",
-                "al:android:package": "com.tumblr",
-                "og:image:type": "image/png",
-                "twitter:app:name:googleplay": "Tumblr",
-                "al:ios:url": "tumblr://x-callback-url/blog?blogName=helloundefined&postID=159394388682",
-                "twitter:app:id:iphone": "305343404",
-                "og:description": "Transgender Care Bear",
-                "og:image:secure_url": "https://64.media.tumblr.com/14a44eb3df693dc2d0091c3751a81c9e/tumblr_oo63nexuWw1vgg8gao1_640.png",
-                "al:ios:app_store_id": "305343404",
-                "twitter:image": "https://64.media.tumblr.com/14a44eb3df693dc2d0091c3751a81c9e/tumblr_oo63nexuWw1vgg8gao1_500.png",
-                "twitter:site": "tumblr",
-                "og:type": "photo",
-                "twitter:title": "helloundefined",
-                "al:ios:app_name": "Tumblr",
-                "og:title": "Queer Unicorn",
-                "og:image:height": "576",
-                "twitter:app:id:ipad": "305343404",
-                "al:android:url": "tumblr://x-callback-url/blog?blogName=helloundefined&postID=159394388682",
-                "fb:app_id": "48119224995",
-                "twitter:app:url:googleplay": "tumblr://x-callback-url/blog?blogName=helloundefined&postID=159394388682&referrer=twitter-cards",
-                "twitter:app:name:ipad": "Tumblr",
-                "viewport": "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
-                "twitter:description": "Transgender Care Bear",
-                "og:url": "https://helloundefined.tumblr.com/post/159394388682/transgender-care-bear",
-                "al:android:app_name": "Tumblr",
-                "twitter:app:name:iphone": "Tumblr"
-            }
-        ],
-        "cse_image": [
-            {
-                "src": "https://64.media.tumblr.com/14a44eb3df693dc2d0091c3751a81c9e/tumblr_oo63nexuWw1vgg8gao1_640.png"
+                "title": "Google Custom Search - tailwind css",
+                "totalResults": "29200000",
+                "searchTerms": "tailwind css",
+                "count": 10,
+                "startIndex": 11,
+                "inputEncoding": "utf8",
+                "outputEncoding": "utf8",
+                "safe": "off",
+                "cx": "897d475f0e4c4881d",
+                "searchType": "image"
             }
         ]
-    }
+    },
+    "context": {
+        "title": "go-v3"
+    },
+    "searchInformation": {
+        "searchTime": 0.552086,
+        "formattedSearchTime": "0.55",
+        "totalResults": "29200000",
+        "formattedTotalResults": "29,200,000"
+    },
+    "items": [
+        {
+            "kind": "customsearch#result",
+            "title": "Tailwind CSS IntelliSense | Laravel News",
+            "htmlTitle": "<b>Tailwind CSS</b> IntelliSense | Laravel News",
+            "link": "https://laravelnews.imgix.net/images/tailwind-intellisense.jpg?ixlib=php-3.3.1",
+            "displayLink": "laravel-news.com",
+            "snippet": "Tailwind CSS IntelliSense | Laravel News",
+            "htmlSnippet": "<b>Tailwind CSS</b> IntelliSense | Laravel News",
+            "mime": "image/jpeg",
+            "fileFormat": "image/jpeg",
+            "image": {
+                "contextLink": "https://laravel-news.com/tailwind-css-intellisense",
+                "height": 1100,
+                "width": 2200,
+                "byteSize": 282961,
+                "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWylgL-5yUIiT2Cwz-j0eu0FMdb--1BbAHgqXrIkXlk5gks2hPtg2Z0CM&s",
+                "thumbnailHeight": 75,
+                "thumbnailWidth": 150
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Designing with Tailwind CSS",
+            "htmlTitle": "Designing with <b>Tailwind CSS</b>",
+            "link": "https://v1.tailwindcss.com/_next/static/media/workcation-screenshot.e15fad2353857da51319620da615ff93.jpg",
+            "displayLink": "v1.tailwindcss.com",
+            "snippet": "Designing with Tailwind CSS",
+            "htmlSnippet": "Designing with <b>Tailwind CSS</b>",
+            "mime": "image/jpeg",
+            "fileFormat": "image/jpeg",
+            "image": {
+                "contextLink": "https://v1.tailwindcss.com/course",
+                "height": 1059,
+                "width": 1473,
+                "byteSize": 159308,
+                "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_G2HsPrg1S6oU3TvAfubMUxncbGChfjrSGmuIlvbqu2CdyuQHoejukQ&s",
+                "thumbnailHeight": 108,
+                "thumbnailWidth": 150
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Tailwind CSS IntelliSense - Visual Studio Marketplace",
+            "htmlTitle": "<b>Tailwind CSS</b> IntelliSense - Visual Studio Marketplace",
+            "link": "https://raw.githubusercontent.com/bradlc/vscode-tailwindcss/master/packages/vscode-tailwindcss/.github/autocomplete.png",
+            "displayLink": "marketplace.visualstudio.com",
+            "snippet": "Tailwind CSS IntelliSense - Visual Studio Marketplace",
+            "htmlSnippet": "<b>Tailwind CSS</b> IntelliSense - Visual Studio Marketplace",
+            "mime": "image/png",
+            "fileFormat": "image/png",
+            "image": {
+                "contextLink": "https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss",
+                "height": 1170,
+                "width": 2080,
+                "byteSize": 261905,
+                "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpMMDcLWzk1A1Aad6iMc1y0kK_fPmKUk4UPRanLgbD1jYa-8vAyy1hN6I&s",
+                "thumbnailHeight": 84,
+                "thumbnailWidth": 150
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Installation: Tailwind CLI - Tailwind CSS",
+            "htmlTitle": "Installation: <b>Tailwind</b> CLI - <b>Tailwind CSS</b>",
+            "link": "https://tailwindcss.com/_next/static/media/social-square.b622e290e82093c36cca57092ffe494f.jpg",
+            "displayLink": "tailwindcss.com",
+            "snippet": "Installation: Tailwind CLI - Tailwind CSS",
+            "htmlSnippet": "Installation: <b>Tailwind</b> CLI - <b>Tailwind CSS</b>",
+            "mime": "image/jpeg",
+            "fileFormat": "image/jpeg",
+            "image": {
+                "contextLink": "https://tailwindcss.com/docs/installation",
+                "height": 1200,
+                "width": 1200,
+                "byteSize": 21851,
+                "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQctSGGENUdrM_-VfQruf8cKZjJpPdDYbl7l8NSADz_VvCPRonlBKC7_IUc&s",
+                "thumbnailHeight": 150,
+                "thumbnailWidth": 150
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Tailwind CSS | WebStorm",
+            "htmlTitle": "<b>Tailwind CSS</b> | WebStorm",
+            "link": "https://resources.jetbrains.com/help/img/idea/2021.3/ws_tailwind_css_preview_completion_dark.png",
+            "displayLink": "www.jetbrains.com",
+            "snippet": "Tailwind CSS | WebStorm",
+            "htmlSnippet": "<b>Tailwind CSS</b> | WebStorm",
+            "mime": "image/png",
+            "fileFormat": "image/png",
+            "image": {
+                "contextLink": "https://www.jetbrains.com/help/webstorm/tailwind-css.html",
+                "height": 656,
+                "width": 1412,
+                "byteSize": 76511,
+                "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO-GDrlFsN9RpT-B16QwBFfvweDcf0TrG-4aRXwnah6NyEuKN-CgOPvQ&s",
+                "thumbnailHeight": 70,
+                "thumbnailWidth": 150
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "File:Tailwind CSS Logo.svg - Wikimedia Commons",
+            "htmlTitle": "File:<b>Tailwind CSS</b> Logo.svg - Wikimedia Commons",
+            "link": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1200px-Tailwind_CSS_Logo.svg.png",
+            "displayLink": "commons.wikimedia.org",
+            "snippet": "File:Tailwind CSS Logo.svg - Wikimedia Commons",
+            "htmlSnippet": "File:<b>Tailwind CSS</b> Logo.svg - Wikimedia Commons",
+            "mime": "image/png",
+            "fileFormat": "image/png",
+            "image": {
+                "contextLink": "https://commons.wikimedia.org/wiki/File:Tailwind_CSS_Logo.svg",
+                "height": 1200,
+                "width": 1200,
+                "byteSize": 34754,
+                "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8fAjVvARrp_F2ObzyIky6qby0diApfGlENNuf8PFM8ZE2gI5SYyfsRJ4&s",
+                "thumbnailHeight": 150,
+                "thumbnailWidth": 150
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Tailwind CSS Starter Kit | Drupal.org",
+            "htmlTitle": "<b>Tailwind CSS</b> Starter Kit | Drupal.org",
+            "link": "https://www.drupal.org/files/project-images/screenshot_361.png",
+            "displayLink": "www.drupal.org",
+            "snippet": "Tailwind CSS Starter Kit | Drupal.org",
+            "htmlSnippet": "<b>Tailwind CSS</b> Starter Kit | Drupal.org",
+            "mime": "image/png",
+            "fileFormat": "image/png",
+            "image": {
+                "contextLink": "https://www.drupal.org/project/tailwindcss",
+                "height": 400,
+                "width": 400,
+                "byteSize": 7158,
+                "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQniGOFIc0T_Pxp6gVSF0zDGIbdKfIUgHYNI0CAlwzuQr76oorrLhnj5A&s",
+                "thumbnailHeight": 124,
+                "thumbnailWidth": 124
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Is Tailwind Really Worth It?. Tailwind, the new kid on the block ...",
+            "htmlTitle": "Is <b>Tailwind</b> Really Worth It?. <b>Tailwind</b>, the new kid on the block ...",
+            "link": "https://miro.medium.com/max/712/0*QXkyD4rFK7ivYf9-.png",
+            "displayLink": "javascript.plainenglish.io",
+            "snippet": "Is Tailwind Really Worth It?. Tailwind, the new kid on the block ...",
+            "htmlSnippet": "Is <b>Tailwind</b> Really Worth It?. <b>Tailwind</b>, the new kid on the block ...",
+            "mime": "image/png",
+            "fileFormat": "image/png",
+            "image": {
+                "contextLink": "https://javascript.plainenglish.io/is-tailwind-css-really-worth-using-1830a706231a",
+                "height": 300,
+                "width": 712,
+                "byteSize": 29672,
+                "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTtrf56RAHvn_-Umj6AcYBYMv6tLY9yobzOvr4eVfuH4Gw2WrNsy6USQ&s",
+                "thumbnailHeight": 59,
+                "thumbnailWidth": 140
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Tailwind CSS Starter Kit | Drupal.org",
+            "htmlTitle": "<b>Tailwind CSS</b> Starter Kit | Drupal.org",
+            "link": "https://www.drupal.org/files/styles/grid-3-2x/public/project-images/screenshot_361.png?itok=w4CzcWyb",
+            "displayLink": "www.drupal.org",
+            "snippet": "Tailwind CSS Starter Kit | Drupal.org",
+            "htmlSnippet": "<b>Tailwind CSS</b> Starter Kit | Drupal.org",
+            "mime": "image/png",
+            "fileFormat": "image/png",
+            "image": {
+                "contextLink": "https://www.drupal.org/project/tailwindcss",
+                "height": 400,
+                "width": 400,
+                "byteSize": 6833,
+                "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe3nPNy8zuCqISZaVsqC-PkpgoozWXn9o4aF-Ar_f1v7NSSlxqWJ9m&s",
+                "thumbnailHeight": 124,
+                "thumbnailWidth": 124
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Material Tailwind Kit React: Free Tailwind CSS & Material Design ...",
+            "htmlTitle": "Material <b>Tailwind</b> Kit React: Free <b>Tailwind CSS</b> &amp; Material Design ...",
+            "link": "https://s3.amazonaws.com/creativetim_bucket/products/486/original/opt_mtr_thumbnail.jpg?1623313854",
+            "displayLink": "www.creative-tim.com",
+            "snippet": "Material Tailwind Kit React: Free Tailwind CSS & Material Design ...",
+            "htmlSnippet": "Material <b>Tailwind</b> Kit React: Free <b>Tailwind CSS</b> &amp; Material Design ...",
+            "mime": "image/jpeg",
+            "fileFormat": "image/jpeg",
+            "image": {
+                "contextLink": "https://www.creative-tim.com/product/material-tailwind-kit-react",
+                "height": 600,
+                "width": 800,
+                "byteSize": 90436,
+                "thumbnailLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRX-wHY-cczVV_RC0ru4LDgYnmVdwBFYOa8qhlWYV0kIhWdjQ44ectkwdM&s",
+                "thumbnailHeight": 107,
+                "thumbnailWidth": 143
+            }
+        }
+    ]
 }
--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,18 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router';
 import SearchHeader from '../Components/SearchHeader'
+import SearchResults from '../Components/SearchResults';
 import Response from '../Response';
 
 const search = ({results}) => {
+  const router = useRouter()
   return (
     <div>
         <Head>
-            <title>Search page</title>
+            <title>{router.query.term} - Search page</title>
         </Head>
             <SearchHeader />
+            <SearchResults results={results}/>
     </div>
   )
 }
