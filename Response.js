b84d2d8090c6cdb2bb347c9facaa8eb2c184626e
amolmugnusmare <amol20@navgurukul.org>
2022-05-13 11:26:24 +0530

    create data from api and mockdata

---
+++ Response.js
@@ -0,0 +1,66 @@
+export default {
+    "kind": "customsearch#result",
+    "title": "Queer Unicorn — Transgender Care Bear",
+    "htmlTitle": "Queer Unicorn — Transgender Care Bear",
+    "link": "https://helloundefined.tumblr.com/post/159394388682/transgender-care-bear",
+    "displayLink": "helloundefined.tumblr.com",
+    "snippet": "Apr 9, 2017 ... p1ss4ss liked this. mintbunny2theface liked this. helloundefined posted this. Show more notesLoading... See more posts like this on Tumblr.",
+    "htmlSnippet": "Apr 9, 2017 <b>...</b> p1ss4ss liked this. mintbunny2theface liked this. <b>helloundefined</b> posted this. Show more notesLoading... See more posts like this on Tumblr.",
+    "cacheId": "eHtmYD_ymVEJ",
+    "formattedUrl": "https://helloundefined.tumblr.com/post/.../transgender-care-bear",
+    "htmlFormattedUrl": "https://<b>helloundefined</b>.tumblr.com/post/.../transgender-care-bear",
+    "pagemap": {
+        "cse_thumbnail": [
+            {
+                "src": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQfE0_M1_TrrBPJcvVl_pju4wsJw4eDs1lqUitbQbsJartTsw2YKG7Fo9Q",
+                "width": "228",
+                "height": "221"
+            }
+        ],
+        "metatags": [
+            {
+                "og:determiner": "a",
+                "apple-itunes-app": "app-id=305343404,affiliate-data=at=11l3M5&ct=us.itunes.uni.tumblr.ipdt,app-argument=tumblr://x-callback-url/blog?blogName=helloundefined&postID=159394388682&referrer=smart-app-banner",
+                "og:image": "https://64.media.tumblr.com/14a44eb3df693dc2d0091c3751a81c9e/tumblr_oo63nexuWw1vgg8gao1_640.png",
+                "twitter:app:id:googleplay": "com.tumblr",
+                "twitter:app:url:iphone": "tumblr://x-callback-url/blog?blogName=helloundefined&postID=159394388682&referrer=twitter-cards",
+                "theme-color": "#35465D",
+                "og:image:width": "592",
+                "twitter:card": "summary_large_image",
+                "og:site_name": "Queer Unicorn",
+                "twitter:url": "https://helloundefined.tumblr.com/post/159394388682/transgender-care-bear",
+                "twitter:app:url:ipad": "tumblr://x-callback-url/blog?blogName=helloundefined&postID=159394388682&referrer=twitter-cards",
+                "al:android:package": "com.tumblr",
+                "og:image:type": "image/png",
+                "twitter:app:name:googleplay": "Tumblr",
+                "al:ios:url": "tumblr://x-callback-url/blog?blogName=helloundefined&postID=159394388682",
+                "twitter:app:id:iphone": "305343404",
+                "og:description": "Transgender Care Bear",
+                "og:image:secure_url": "https://64.media.tumblr.com/14a44eb3df693dc2d0091c3751a81c9e/tumblr_oo63nexuWw1vgg8gao1_640.png",
+                "al:ios:app_store_id": "305343404",
+                "twitter:image": "https://64.media.tumblr.com/14a44eb3df693dc2d0091c3751a81c9e/tumblr_oo63nexuWw1vgg8gao1_500.png",
+                "twitter:site": "tumblr",
+                "og:type": "photo",
+                "twitter:title": "helloundefined",
+                "al:ios:app_name": "Tumblr",
+                "og:title": "Queer Unicorn",
+                "og:image:height": "576",
+                "twitter:app:id:ipad": "305343404",
+                "al:android:url": "tumblr://x-callback-url/blog?blogName=helloundefined&postID=159394388682",
+                "fb:app_id": "48119224995",
+                "twitter:app:url:googleplay": "tumblr://x-callback-url/blog?blogName=helloundefined&postID=159394388682&referrer=twitter-cards",
+                "twitter:app:name:ipad": "Tumblr",
+                "viewport": "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+                "twitter:description": "Transgender Care Bear",
+                "og:url": "https://helloundefined.tumblr.com/post/159394388682/transgender-care-bear",
+                "al:android:app_name": "Tumblr",
+                "twitter:app:name:iphone": "Tumblr"
+            }
+        ],
+        "cse_image": [
+            {
+                "src": "https://64.media.tumblr.com/14a44eb3df693dc2d0091c3751a81c9e/tumblr_oo63nexuWw1vgg8gao1_640.png"
+            }
+        ]
+    }
+}
--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router';
 import SearchHeader from '../Components/SearchHeader'
+import Response from '../Response';
 
-const search = ({}) => {
-
+const search = ({results}) => {
   return (
     <div>
         <Head>
@@ -17,23 +17,17 @@ const search = ({}) => {
 
 
 export async function getServerSideProps(context) {
-  const startIndex = context.query.start || "1";
-  const mockData = false;
-  const data = mockData
-    ? Response
-    : await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${
-          process.env.API_KEY
-        }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
-          context.query.searchType && "&searchType=image"
-        }&start=${startIndex}`
-      ).then((response) => response.json());
-  return {
-    props: {
-      results: data,
-    },
+  const mockData = true
+  const data =mockData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}`
+  ).then((response) => response.json());
+
+  return{
+    props:{
+      results:data
+    }
+  }
   };
-}
+
 
 
 export default search
